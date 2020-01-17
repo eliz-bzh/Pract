b7fd32200b1c2fd976bf2d31310dc60230d9bab5
Liz <eliz.com>
2020-01-17 18:34:49 +0300

    1-20

--- Practica/Program.cs
+++ Practica/Program.cs
@@ -10,32 +10,76 @@ namespace Practica
     {
         static void Main(string[] args)
         {
-            //Students students = new Students();
+            //Students students = new Students(new List<Student>
+            //{
+            //    new Student("Bozh", 795, new int[5] { 2,3,5, 5,4 }),
+            //    new Student("Korob", 795, new int[5] { 5,6,4, 9,2 }),
+            //    new Student("Vg", 795, new int[5] { 10,3,5, 5,8 })
+            //});
             //students.AverageMarkGreater4();
 
-            //AER0FL0TS tS = new AER0FL0TS();
-            //tS.PrintDependentNumber();
+            //AER0FL0TS tS = new AER0FL0TS(new List<AER0FL0T>
+            //{
+            //    new AER0FL0T("Bozh", 2019, "1 sort"),
+            //    new AER0FL0T("Korob", 2016, "2 sort"),
+            //    new AER0FL0T("Vg", 2013, "1 sort")
+            //});
+            //tS.DependentTypePlain("1 sort");
 
-            //Workers workers = new Workers();
+            //Workers workers = new Workers(new List<Worker>
+            //{
+            //    new Worker("Bozh", "1 sort", 2019),
+            //    new Worker("Korob", "2 sort", 2016),
+            //    new Worker("Vg", "1 sort", 2013),
+            //});
             //workers.PrintDependentExperience(2);
 
-            //Trains trains = new Trains();
+            //var t = DateTime.Now;
+            //Trains trains = new Trains(new List<Train>
+            //{
+            //    new Train("Minsk", 222, new DateTime(t.Year, t.Month, t.Day, 3, 22, 32)),
+            //    new Train("LA", 342, new DateTime(t.Year, t.Month, t.Day, 22, 33, 7)),
+            //});
             //trains.PrintDependentDestination("Minsk");
 
-            //Marches march = new Marches();
-            //march.PrintDependentNomerMarshryta(34);
+            //Marches march = new Marches(new List<March>
+            //{
+            //    new March("Minsk", "Moscow", 345),
+            //    new March("Moscow", "Minsk", 2331),
+            //    new March("Minsk", "LA", 3333)
+            //});
+            //march.PrintDependentNomerMarshryta(345);
 
-            //Notes notes = new Notes();
-            //notes.PrintDependentSurname("B");
+            //Notes notes = new Notes(new List<Note>
+            //{
+            //    new Note("Bozh", "Liz", "+375332224443", new int[3] { 2,3,5}),
+            //    new Note("Korob", "Dasha", "+375332434343", new int[3] { 3,4,5})
+            //});
+            //notes.PrintDependentSurname("Bozh");
 
-            //Znaks znaks = new Znaks();
-            //znaks.PrintDependentSurname("XZ");
+            //Znaks znaks = new Znaks(new List<Znak>
+            //{
+            //    new Znak("var", "sss", "sww", new int[3] {3,4,5}),
+            //    new Znak("ddd", "fefs", "sdkfww", new int[3] {3,46,52}),
+            //    new Znak("vaff", "ssllls", "sdddww", new int[3] {36,477,23}),
+            //});
+            //znaks.PrintDependentSurname("sss");
 
-            //Prices prices = new Prices();
-            //prices.PrintDependentName("KKK");
+            //Prices prices = new Prices(new List<Price>
+            //{
+            //    new Price("comp", "texno", 456),
+            //    new Price("eat", "eatShop", 34),
+            //    new Price("car", "carsShop", 34567)
+            //});
+            //prices.PrintDependentName("car");
 
-            Orders orders = new Orders();
-            orders.PrintDependentPlatelshik(123);
+            //Orders orders = new Orders(new List<Order>
+            //{
+            //    new Order(34,33,345),
+            //    new Order(34222,33112,3412125),
+            //    new Order(3114,33111,341215),
+            //});
+            //orders.PrintDependentPlatelshik(34);
 
             Console.ReadKey();
         }
